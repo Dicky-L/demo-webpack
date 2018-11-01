eb285b292ba4f5c41cd6d5208f8c4b4505d761f2
Dicky Liu <dickyliu@DickydeMacBook-Pro.local>
2018-11-01 15:05:42 +0800

    asset management

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import _ from 'lodash'
+import "./style.css"
+import myImage from "./icon_h5.png"
 
 function component() {
   var element = document.createElement('div');
 
   // Lodash 现在通过本script中导入
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
+
+  // 将图像添加到我们现有的 div。
+  var myIcon = new Image();
+  myIcon.src = myImage;
+
+  element.appendChild(myIcon);
 
   return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-const path = require('path');
+const path = require('path')
 
 module.exports = {
   mode: 'development',
@@ -6,5 +6,19 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      }
+    ]
   }
-};
+}
