838c934f0b4ad9b5f97258968bbd8334a35e16c1
Dicky Liu <dickyliu@DickydeMacBook-Pro.local>
2018-11-04 22:19:29 +0800

    file out demo

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import _ from 'lodash'
+import printMe from './print.js'
 
 function component() {
-  var element = document.createElement('div');
+  var element = document.createElement('div')
+  var btn = document.createElement('button')
 
   // Lodash 现在通过本script中导入
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ')
 
-  return element;
+  btn.innerHTML = 'Click me and check the console'
+  btn.onclick = printMe
+
+  element.appendChild(btn)
+
+  return element
 }
 
-document.body.appendChild(component());
+document.body.appendChild(component())
